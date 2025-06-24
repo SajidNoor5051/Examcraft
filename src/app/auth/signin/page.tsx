b95f44fab510b23f635440a62f4bdc8053885587
SajidNoor5051 <sajidmostafiznoor@gmail.com>
2025-06-25 02:52:45 +0600

    Initial commit

--- src/app/auth/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SignInForm } from "@/components/features/auth/SignInForm";
-
-export default function SignInPage() {
-  return <SignInForm />;
-}
--- src/app/auth/signup/page.tsx
+++
@@ -1,5 +0,0 @@
-import { SignUpForm } from "@/components/features/auth/SignUpForm";
-
-export default function SignUpPage() {
-  return <SignUpForm />;
-}
